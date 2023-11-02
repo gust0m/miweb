59fe8910718b7274f9e0a0b9f9b30e5fb957b919
gustom <joseadriantorres2002@gmail.com>
2023-11-02 07:07:41 -0600

    agregando carpeta backend

---
+++ Backend/type_hints.py
@@ -0,0 +1,9 @@
+### type Hints ###
+
+my_string_variable = "my_string_variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_int_variable = 5
+print(my_int_variable)
+print(type(my_int_variable))
