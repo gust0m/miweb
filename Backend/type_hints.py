8ba15477999cca6ffba645ba9b0e9dd8edd03a6d
gustom <joseadriantorres2002@gmail.com>
2023-11-02 07:35:07 -0600

    varibles de dinamicas parte 2

--- Backend/type_hints.py
+++ Backend/type_hints.py
@@ -8,3 +8,11 @@ my_int_variable = 5
 print(my_int_variable)
 print(type(my_int_variable))
 
+my_type_varible: int = "my type String variable"
+print(my_type_varible)
+print(type(my_type_varible))
+
+my_type_varible = 5
+print(my_type_varible)
+print(type(my_type_varible))
+
